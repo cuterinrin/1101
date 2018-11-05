eb8974b2bf9f6856b2b801e17106bc8d466d208c
cuterinrin <yro7045@naver.com>
2018-11-06 02:24:55 +0900

    1101-2nd-bu

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define N_SORT 5
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a, b, c, d;
+	int nums[N_SORT];
+	int i;
 
-	printf("숫자 4개를 입력하세요. : ");
-	scanf("%i %i %i %i", &a, &b, &c, &d);
-	
+	printf("input %i numbers : ", N_SORT);
+	for (i=0; i<N_SORT; i++)
+	{
+	 scanf("%d", &nums[i]);
+	}
 
 	//버블소트 함수 짜서 넣기 
 	
-	printf("오름차순 정렬 : %i %i %i %i", a, b, c, d);
+	printf("sorted numbers are : ", N_SORT);
+	for (i=0; i<N_SORT; i++)
+	{
+	 printf("%d ", &nums[i]);
+	}
 	
 	return 0;
 }
